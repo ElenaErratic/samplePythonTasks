b30fcb13352e809337ff5f3febdee956283da9e8
ElenaErratic <etikhomirova@gmail.com>
2022-01-18 23:49:24 +0300

    added draft stage 2 for regression

--- linear_regression/regression.py
+++ linear_regression/regression.py
@@ -1,14 +1,16 @@
+import numpy as np
+import pandas as pd
 from scipy.stats import linregress
 
 """
-Stage 1 of 4
-Task: Calculate the linear regression for the dataset and print the intercept and coefficient.
-Required result format: {'Intercept': 2.0, 'Coefficient': array([2.])}
-https://hyperskill.org/projects/195/stages/971/implement"""
+Stage 2 of 4
+Task: Calculate multiple linear regression for the dataset and print the coefficients.
+https://hyperskill.org/projects/195/stages/972/implement"""
 
-x = [4.0, 4.5, 5, 5.5, 6.0, 6.5, 7.0]
-y = [33, 42, 45, 51, 53, 61, 62]
-res = linregress(x, y)
+df = pd.DataFrame({'y': [33, 42, 45, 51, 53, 61, 62],
+                   'x': [4, 4.5, 5, 5.5, 6, 6.5, 7],
+                   'w': [1, -3, 2, 5, 0, 3, 6],
+                   'z': [11, 15, 12, 9, 18, 13, 16]})
 
 
 class CustomLinearRegression:
@@ -16,15 +18,25 @@ class CustomLinearRegression:
     def __init__(self, *, fit_intercept=True):
 
         self.fit_intercept = True
-        self.coefficient = res[0]
-        self.intercept = res[1]
+        self.coefficient = None
+        self.intercept = None
 
     def fit(self, X, y):
-        self.X = X
-        self.y = y
-        pass
-        print(f"{{'Intercept': {self.coefficient}, 'Coefficient': {self.intercept}}}")
-        # {'Intercept': 2.0, 'Coefficient': array([2.])}
+        if self.fit_intercept:
 
-dataset = CustomLinearRegression(fit_intercept=True)
-dataset.fit(x, y)
+            self.X = X
+            self.y = y
+            res = linregress(X, y)
+            self.coefficient = res[0]  # np.array(res[0])
+            self.intercept = res[1]
+            print(f"{{'Intercept': {self.intercept}, 'Coefficient': array([{self.coefficient}])}}")
+        else:
+            pass  # todo
+
+
+    def predict(self, X):
+        pass
+
+regCustom = CustomLinearRegression(fit_intercept=False)
+regCustom.fit(df[['x', 'w', 'z']], df['y'])
+y_pred = regCustom.predict(df[['x', 'w', 'z']])
